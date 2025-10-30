e0675d5f17e1f0509f89d067c811191164fabf17
Ramiro Calaza <ramiro.calaza@gmail.com>
2025-10-29 21:21:27 -0300

    Agregado de entidades (#6)
    * wip
    
    * add entyties
    
    ---------
    
    Co-authored-by: Ramiro Calaza <Ramiro.Calaza@boldt.com.ar>
    Co-authored-by: Sofía Belén Cartacci <90361692+sofiacartacci@users.noreply.github.com>

---
+++ CampusShare.Web/Models/Admin.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public class Admin : User
+{
+   
+
+    public Admin(string id, string nombre, string apellido, string dni, string email)
+        : base(id, nombre, apellido, dni, email)
+   
+}
